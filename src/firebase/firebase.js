dc817062f9a715664075fe2fc21d125e33170ead
Cholpon <januzakova.cholpon@gmail.com>
2026-04-15 21:29:39 +0600

    feat: add Firebase SDK and initialize Firebase in the project
    - Added Firebase dependency to package.json
    - Created firebase.js to initialize Firebase with the provided configuration

---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDt_rvOrOeymVaN86RqM2ssEM4CIsxlCQ",
+  authDomain: "praktika-ee8f2.firebaseapp.com",
+  projectId: "praktika-ee8f2",
+  storageBucket: "praktika-ee8f2.firebasestorage.app",
+  messagingSenderId: "558735626311",
+  appId: "1:558735626311:web:48bc69b0b3fdc411366162",
+  measurementId: "G-LZZ6V98XHE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
